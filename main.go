c1a4dc883a1a3b732cdcfc575f886c7b6fdee25d
soulteary <soulteary@gmail.com>
2023-05-23 10:47:30 +0800

    chore: refactor Get the fellows list from the page

--- main.go
+++ main.go
@@ -1,80 +1,65 @@
 package main
 
 import (
-	"encoding/csv"
+	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/serenize/snaker"
 )
 
 type Fellow struct {
-	Name string
-	Year string
+	Name   string
+	Year   string
+	URL    string
+	Region string
 }
 
-func main() {
-	// origin page addr: https://awards.acm.org/fellows/award-winners
-	quotePage := "https://awards.acm.org/fellows/award-recipients"
-	resp, err := http.Get(quotePage)
+func GetListFromPage(link string) (fellows []Fellow, error error) {
+	resp, err := http.Get(link)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	names := make(map[string]string)
-
-	doc.Find("tr[role=row]").Each(func(i int, row *goquery.Selection) {
-		name := row.Find("td:nth-child(1) a").Text()
-		year := row.Find("td[role=rowheader]").Text()
-		if year == "" {
-			year = "0"
+	doc.Find("table tbody tr[role=row]").Each(func(_ int, row *goquery.Selection) {
+		cells := row.Find("td")
+		if cells.Length() != 5 {
+			return
 		}
-		name = processName(name)
-		names[name] = year
+
+		fellow := Fellow{
+			Name:   strings.TrimSpace(cells.Eq(0).Find("a").Text()),
+			URL:    cells.Eq(0).Find("a").AttrOr("href", ""),
+			Year:   strings.TrimSpace(cells.Eq(2).Text()),
+			Region: strings.TrimSpace(cells.Eq(3).Text()),
+		}
+
+		if fellow.Year == "" {
+			fellow.Year = "0"
+		}
+
+		fellows = append(fellows, fellow)
 	})
 
-	err = writeCSV(names)
+	return fellows, nil
+}
+
+func main() {
+	// origin page addr: https://awards.acm.org/fellows/award-winners
+	fellows, err := GetListFromPage("https://awards.acm.org/fellows/award-recipients")
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func processName(name string) string {
-	name = strings.TrimSpace(name)
-	name = snaker.SnakeToCamel(name)
-	return name
-}
-
-func writeCSV(names map[string]string) error {
-	file, err := os.Create("acm-fellows.csv")
-	if err != nil {
-		return err
+	fmt.Println("Total fellows:", len(fellows))
+	for _, fellow := range fellows {
+		fmt.Println(fellow)
 	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	err = writer.Write([]string{"name", "year"})
-	if err != nil {
-		return err
-	}
-
-	for name, year := range names {
-		err := writer.Write([]string{name, year})
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
