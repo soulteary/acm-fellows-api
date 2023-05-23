2aa40dbc1052319e9843c1fd811759dce2d02a5f
soulteary <soulteary@gmail.com>
2023-05-23 10:20:10 +0800

    feat: inital project with ChatGPT refactor the https://github.com/emeryberger/CSrankings/blob/gh-pages/util/acm-fellow-scraper.py

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/soulteary/acm-fellows-api
+
+go 1.20
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/serenize/snaker v0.0.0-20201027110005-a7ad2135616e
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.7 // indirect
+	golang.org/x/net v0.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/serenize/snaker"
+)
+
+type Fellow struct {
+	Name string
+	Year string
+}
+
+func main() {
+	quotePage := "https://awards.acm.org/fellows/award-winners"
+	resp, err := http.Get(quotePage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	names := make(map[string]string)
+
+	doc.Find("tr[role=row]").Each(func(i int, row *goquery.Selection) {
+		name := row.Find("td:nth-child(1) a").Text()
+		year := row.Find("td[role=rowheader]").Text()
+		if year == "" {
+			year = "0"
+		}
+		name = processName(name)
+		names[name] = year
+	})
+
+	err = writeCSV(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func processName(name string) string {
+	name = strings.TrimSpace(name)
+	name = snaker.SnakeToCamel(name)
+	return name
+}
+
+func writeCSV(names map[string]string) error {
+	file, err := os.Create("acm-fellows.csv")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	err = writer.Write([]string{"name", "year"})
+	if err != nil {
+		return err
+	}
+
+	for name, year := range names {
+		err := writer.Write([]string{name, year})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
